8b7776a8e30e7d152fda3f5a6619e41afcdb9a75
pasca-l <shion.yamadate@keio.jp>
2023-07-27 21:58:29 +0900

    updated notes on generics

--- languages/go/sandbox/src/generics.go
+++ languages/go/sandbox/src/generics.go
@@ -19,15 +19,36 @@ func Index[T comparable](s []T, x T) int {
 	return -1
 }
 
-// generic types can implement generic data structures
+// to allow the constraint to include the original type definition,
+// ~ needs to be added infront of the type
+// eg. without ~, "CustomInt" to a type parameter of "Number" raises an error,
+//     but with ~, as "CutomInt" is originally "int" type, the error resolves
+type Number interface {
+	~int | int32 | int64 | float32 | float64
+}
+
+func PrintNumber[T Number](n T) {
+	fmt.Println(n)
+}
+
+// generic types can implement generic data structures,
+// however method for these structures can not hold type parameters
 // eg. singly-linked list holding any type of value
 type List[T any] struct {
 	next *List[T]
 	val  T
 }
 
+// generic types can also be used for type definitions
+type Vector[T any] []T
+type VectorInt = Vector[int]
+
 func main() {
 	si := []int{10, 20, 15, -10}
 	ss := []string{"foo", "bar", "baz"}
 	fmt.Println(Index(si, 15), Index(ss, "hello"))
+
+	type CustomInt int
+	var x CustomInt = 1
+	PrintNumber[CustomInt](x)
 }
