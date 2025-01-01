b3edc101316752182378d5ad429310ec5a59c7d9
pasca-l <shion.yamadate@keio.jp>
2025-01-01 15:48:40 +0900

    added notes on ownership

---
+++ languages/rust/basics/ownership.rs
@@ -0,0 +1,8 @@
+// referenced "Understanding Ownership" (https://doc.rust-lang.org/book/ch04-00-understanding-ownership.html)
+
+// There are three ownership rules:
+// 1. each value in Rust has an owner
+// 2. there can only be one owner at a time
+// 3. when the owner goes out of scope, the value is dropped
+
+
